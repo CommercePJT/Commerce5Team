507834ded256fa40b49164ae108036cd3ab9685a
ihateahri <ihateahri@naver.com>
2026-07-13 14:35:46 +0900

    고객 조회, 수정, 상태 변경, 삭제 컨트롤러 구현

--- src/main/java/com/example/commercepjt/customer/controller/CustomerController.java
+++ src/main/java/com/example/commercepjt/customer/controller/CustomerController.java
@@ -1,9 +1,12 @@
 package com.example.commercepjt.customer.controller;
 
+import com.example.commercepjt.customer.dto.CustomerResponse;
+import com.example.commercepjt.customer.dto.UpdateCustomerRequest;
+import com.example.commercepjt.customer.dto.UpdateCustomerStatusRequest;
 import com.example.commercepjt.customer.service.CustomerService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/customers")
@@ -12,5 +15,29 @@ public class CustomerController {
 
     private final CustomerService customerService;
 
+    @GetMapping("/{customerId}")
+    public CustomerResponse findOne(@PathVariable Long customerId) {
+        return customerService.findOne(customerId);
+    }
+
+    @PutMapping("/{customerId}")
+    public CustomerResponse update(
+            @Valid @PathVariable Long customerId,
+            @RequestBody UpdateCustomerRequest request
+            ) {
+        return customerService.update(customerId, request);
+    }
+
+    @PatchMapping("/{customerId}/status")
+    public CustomerResponse updateStatus(
+            @PathVariable Long customerId,
+            @RequestBody UpdateCustomerStatusRequest request) {
+        return customerService.updateStatus(customerId, request);
+    }
+
+    @DeleteMapping("/{customerId}")
+    public void delete(@PathVariable Long customerId) {
+        customerService.delete(customerId);
+    }
 
 }
