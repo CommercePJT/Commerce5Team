060f6f30d5b81e523504e80dff2acfa58926d6bd
ihateahri <ihateahri@naver.com>
2026-07-13 17:14:01 +0900

    고객 조회 및 페이징 기능 구현

--- src/main/java/com/example/commercepjt/customer/controller/CustomerController.java
+++ src/main/java/com/example/commercepjt/customer/controller/CustomerController.java
@@ -1,14 +1,18 @@
 package com.example.commercepjt.customer.controller;
 
+import com.example.commercepjt.customer.dto.CustomerListResponse;
 import com.example.commercepjt.customer.dto.CustomerResponse;
 import com.example.commercepjt.customer.dto.UpdateCustomerRequest;
 import com.example.commercepjt.customer.dto.UpdateCustomerStatusRequest;
 import com.example.commercepjt.customer.service.CustomerService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/customers")
@@ -17,11 +21,21 @@ public class CustomerController {
 
     private final CustomerService customerService;
 
+    // 고객 상세 조회
     @GetMapping("/{customerId}")
     public ResponseEntity<CustomerResponse> findOne(@PathVariable Long customerId) {
         return ResponseEntity.ok(customerService.findOne(customerId));
     }
 
+    // 고객 리스트 조회
+    @GetMapping
+    public ResponseEntity<CustomerListResponse> findAll(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        return ResponseEntity.ok(customerService.findAll(page, size));
+    }
+
+    // 고객 정보 수정
     @PutMapping("/{customerId}")
     public ResponseEntity<CustomerResponse> update(
             @Valid @PathVariable Long customerId,
@@ -30,6 +44,7 @@ public class CustomerController {
         return ResponseEntity.ok(customerService.update(customerId, request));
     }
 
+    // 고객 상태 변경
     @PatchMapping("/{customerId}/status")
     public ResponseEntity<CustomerResponse> updateStatus(
             @PathVariable Long customerId,
@@ -37,6 +52,7 @@ public class CustomerController {
         return ResponseEntity.ok(customerService.updateStatus(customerId, request));
     }
 
+    // 고객 삭제
     @DeleteMapping("/{customerId}")
     public ResponseEntity<Void> delete(@PathVariable Long customerId) {
         customerService.delete(customerId);
--- src/main/java/com/example/commercepjt/customer/service/CustomerService.java
+++ src/main/java/com/example/commercepjt/customer/service/CustomerService.java
@@ -1,5 +1,7 @@
 package com.example.commercepjt.customer.service;
 
+import com.example.commercepjt.common.dto.PageInfo;
+import com.example.commercepjt.customer.dto.CustomerListResponse;
 import com.example.commercepjt.customer.dto.CustomerResponse;
 import com.example.commercepjt.customer.dto.UpdateCustomerRequest;
 import com.example.commercepjt.customer.dto.UpdateCustomerStatusRequest;
@@ -8,8 +10,14 @@ import com.example.commercepjt.customer.entity.CustomerStatus;
 import com.example.commercepjt.customer.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,12 +25,40 @@ public class CustomerService {
 
     private final CustomerRepository customerRepository;
 
+    // 고객 상세 조회
     @Transactional(readOnly = true)
     public CustomerResponse findOne(Long customerId) {
         Customer customer = customerOrElseThrow(customerId);
         return new CustomerResponse(customer);
     }
 
+    // 고객 리스트 조회
+    @Transactional(readOnly = true)
+    public CustomerListResponse findAll(int page, int size) {
+        Pageable pageable = PageRequest.of(page - 1, size);
+
+        List<CustomerResponse> responseList = new ArrayList<>();
+
+        // 페이지 정보와 함께 고객 목록 조회
+        Page<Customer> customerPage = customerRepository.findAll(pageable);
+
+
+        for(Customer customer : customerPage.getContent()) {
+            CustomerResponse customerResponse = new CustomerResponse(customer);
+            responseList.add(customerResponse);
+        }
+
+        // 페이지 정보를 PageInfo DTO에 저장
+        PageInfo pageInfo = new PageInfo(
+                customerPage.getNumber() + 1,
+                customerPage.getSize(),
+                customerPage.getTotalElements(),
+                customerPage.getTotalPages()
+        );
+        return new CustomerListResponse(responseList, pageInfo);
+    }
+
+    // 고객 정보 수정
     @Transactional
     public CustomerResponse update(Long customerId, UpdateCustomerRequest request) {
         Customer customer = customerOrElseThrow(customerId);
@@ -32,6 +68,7 @@ public class CustomerService {
         return new CustomerResponse(customer);
     }
 
+    // 고객 상태 변경
     @Transactional
     public CustomerResponse updateStatus(
             Long customerId,
@@ -41,6 +78,7 @@ public class CustomerService {
         return new CustomerResponse(customer);
     }
 
+    // 고객 삭제
     @Transactional
     public void delete(Long customerId) {
         Customer customer = customerOrElseThrow(customerId);
