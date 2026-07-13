bf2f2e32d09f2541aee17a4bb72fb7bb07a57c94
thal070909 <thal070909@gmail.com>
2026-07-13 19:06:33 +0900

    feat: 리뷰 엔티티 및 DTO 구현

--- src/main/java/com/example/commercepjt/review/dto/ReviewDetailResponse.java
+++ src/main/java/com/example/commercepjt/review/dto/ReviewDetailResponse.java
@@ -1,4 +1,30 @@
 package com.example.commercepjt.review.dto;
 
+import com.example.commercepjt.review.entity.Review;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
 public class ReviewDetailResponse {
+
+    private String productName;
+    private String customerName;
+    private String customerEmail;
+    private LocalDateTime createdAt;
+    private int rating;
+    private String content;
+
+    public static ReviewDetailResponse from(Review review) {
+        return new ReviewDetailResponse(
+                review.getOrder().getProduct().getProductName(),
+                review.getOrder().getCustomer().getName(),
+                review.getOrder().getCustomer().getEmail(),
+                review.getCreatedAt(),
+                review.getRating(),
+                review.getContent()
+        );
+    }
 }
--- src/main/java/com/example/commercepjt/review/dto/ReviewResponse.java
+++ src/main/java/com/example/commercepjt/review/dto/ReviewResponse.java
@@ -1,4 +1,32 @@
 package com.example.commercepjt.review.dto;
 
+import com.example.commercepjt.review.entity.Review;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
 public class ReviewResponse {
+
+    private Long reviewId;
+    private String orderNumber;
+    private String customerName;
+    private String productName;
+    private int rating;
+    private String content;
+    private LocalDateTime createdAt;
+
+    public static ReviewResponse from(Review review) {
+        return new ReviewResponse(
+                review.getReviewId(),
+                review.getOrder().getOrderNumber(),
+                review.getOrder().getCustomer().getName(),
+                review.getOrder().getProduct().getProductName(),
+                review.getRating(),
+                review.getContent(),
+                review.getCreatedAt()
+        );
+    }
 }
