fca64c2eecb29f8e271587064dae443c4a4fac4d
최정윤 <jes2ngyun@gmail.com>
2026-07-14 14:38:55 +0900

    "feat: 고객 상세 조회 기능 확인 및 검색 실행 인터페이스 추가"

--- src/main/java/com/example/commercepjt/customer/repository/CustomerRepository.java
+++ src/main/java/com/example/commercepjt/customer/repository/CustomerRepository.java
@@ -2,10 +2,11 @@ package com.example.commercepjt.customer.repository;
 
 import com.example.commercepjt.customer.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
+public interface CustomerRepository extends JpaRepository<Customer, Long>, JpaSpecificationExecutor<Customer> {
 
-    boolean existsByEmailAndCustomerIdNot(
+    boolean existsByEmailAndCustomerIdNot(           //  SELECT COUNT(*) > 0 FROM customers WHERE email = ? AND customer_id != ?
             String email,
             Long customerId
     );
