72c0f5c2e6332a5de5dde740e57d7fc1fca52c17
t2025-m0237 <asd@asad>
2026-07-15 20:09:13 +0900

    UpdateRoleRequest NouNull로 수정

--- src/main/java/com/example/commercepjt/admin/dto/request/UpdateRoleRequest.java
+++ src/main/java/com/example/commercepjt/admin/dto/request/UpdateRoleRequest.java
@@ -1,13 +1,13 @@
 package com.example.commercepjt.admin.dto.request;
 
 import com.example.commercepjt.admin.entity.AdminRole;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
-@Getter
+import jakarta.validation.constraints.NotNull;
 
+@Getter
 public class UpdateRoleRequest {
 
-    @NotBlank(message = "관리자 역할은 필수입니다.")
+    @NotNull(message = "관리자 역할은 필수입니다.")
     private AdminRole role;
 }
