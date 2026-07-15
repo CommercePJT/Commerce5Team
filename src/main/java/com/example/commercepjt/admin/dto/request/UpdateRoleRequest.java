228661564ce2209f54fb64a81ec1468d847fc20e
tls123qwe <tls123qwe@gmail.com>
2026-07-15 20:28:07 +0900

    관리자 역할 및 상태 오류 수정

--- src/main/java/com/example/commercepjt/admin/dto/request/UpdateRoleRequest.java
+++ src/main/java/com/example/commercepjt/admin/dto/request/UpdateRoleRequest.java
@@ -2,12 +2,13 @@ package com.example.commercepjt.admin.dto.request;
 
 import com.example.commercepjt.admin.entity.AdminRole;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 
 public class UpdateRoleRequest {
 
-    @NotBlank(message = "관리자 역할은 필수입니다.")
+    @NotNull(message = "관리자 역할은 필수입니다.")
     private AdminRole role;
 }
