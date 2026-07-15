ed62566b5e8c1d7c727cbd5b1b8bfbb62550c25c
tls123qwe <tls123qwe@gmail.com>
2026-07-15 14:44:08 +0900

    최정이 님이 변경한 내용

--- src/main/java/com/example/commercepjt/auth/dto/response/SignupAdminResponse.java
+++ src/main/java/com/example/commercepjt/auth/dto/response/SignupAdminResponse.java
@@ -1,34 +1,28 @@
 package com.example.commercepjt.auth.dto.response;
 
 import com.example.commercepjt.admin.entity.Admin;
-import com.example.commercepjt.admin.entity.AdminRole;
-import com.example.commercepjt.admin.entity.AdminStatus;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
-@AllArgsConstructor // 모든 필드를 받는 생성자 생성
 public class SignupAdminResponse {
 
     private final Long id;
     private final String name;
     private final String email;
     private final String phone;
-    private final AdminRole role;
-    private final AdminStatus status;
+    private final String role;
+    private final String status;
     private final LocalDateTime createdAt;
 
-
-    public static SignupAdminResponse from(Admin admin) {
-        return new SignupAdminResponse(
-                admin.getId(),
-                admin.getName(),
-                admin.getEmail(),
-                admin.getPhone(),
-                admin.getRole(),
-                admin.getStatus(),
-                admin.getCreatedAt());
+    public SignupAdminResponse(Admin admin) {
+        this.id = admin.getId();
+        this.name = admin.getName();
+        this.email = admin.getEmail();
+        this.phone = admin.getPhone();
+        this.role = admin.getRole().name();
+        this.status = admin.getStatus().name();
+        this.createdAt = admin.getCreatedAt();
     }
 }
